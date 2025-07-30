12593486bb32fb4e9a51e3cf5d52c0da1ee7bcf7
karen <ana_karen_jasso@outlook.com>
2025-07-30 10:37:21 -0600

    add section vida academica

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function animarContadoresEn(seccion) {
+  const counters = seccion.querySelectorAll('.contador');
+  console.log(counters);
+  
+  counters.forEach(counter => {
+    const valorFinal = +counter.getAttribute('data-contar');
+    let valorActual = 0;
+
+    counter.innerText = 0;
+
+    const actualizar = () => {
+      const incremento = Math.ceil(valorFinal / 400);
+
+      if (valorActual < valorFinal) {
+        valorActual += incremento;
+        counter.innerText = valorActual;
+        setTimeout(actualizar, 30);
+      } else {
+        counter.innerText = valorFinal;
+      }
+    };
+
+    actualizar();
+  });
+}
+
+const cooldowns = new WeakMap();
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const seccion = entry.target;
+
+    if (entry.isIntersecting && !cooldowns.get(seccion)) {
+      animarContadoresEn(seccion);
+      cooldowns.set(seccion, true);
+      setTimeout(() => cooldowns.set(seccion, false), 2000);
+    }
+  });
+}, { threshold: 0.4 });
+
+
+document.querySelectorAll('#seccion-contadores').forEach(seccion => {
+  observer.observe(seccion);
+});
+
+
